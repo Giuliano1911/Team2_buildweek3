25f344355e68084af477d0c31eaa615894fb8294
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-12-16 17:55:17 +0100

    Update NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,15 @@
-import { Container, Alert, Button } from 'react-bootstrap'
+import { Container, Alert, Button, Row } from 'react-bootstrap'
 import { useNavigate } from 'react-router'
 
 const NotFound = () => {
   const navigate = useNavigate()
 
   return (
-    <Container className="text-center mt-4">
-      <Alert variant="danger">Pagina non trovata!</Alert>
+    <Container className="text-center mt-5">
+      <Row className="mt-5"></Row>
+      <Alert className="mt-5" variant="danger">
+        Pagina non trovata!
+      </Alert>
       <Button
         variant="success"
         onClick={() => {
